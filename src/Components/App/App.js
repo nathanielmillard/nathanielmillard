320b7a069c2b95d09c6639590d9a03cc137732e7
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-18 17:31:30 -0700

    Impliment router in App.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.scss';
-
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import NavBar from '../NavBar/NavBar.js'
+import Blurb from '../Blurb/Blurb.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Testing</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <main className="App">
+        <NavBar/>
+        <Switch>
+          <Route path='/resume'>
+          </Route>
+          <Route path='/work'>
+          </Route>
+          <Route path='/background'>
+          </Route>
+          <Route path='/contact'>
+          </Route>
+          <Route path='/'>
+            <Blurb />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
