3eccb1c6902c29f8e625bc9a04082e610c85fe7c
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-18 17:32:39 -0700

    Remove misplace code.

--- src/Components/NavBar/NavBar.test.js
+++ src/Components/NavBar/NavBar.test.js
@@ -1,14 +1,1 @@
-import React from 'react';
-import './NavBar.scss';
 
-function NavBar() {
-  return (
-    <div className="NavBar">
-      <header className="NavBar-header">
-        <h1>Testing</h1>
-      </header>
-    </div>
-  );
-}
-
-export default NavBar;
