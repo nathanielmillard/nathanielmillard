ac4948eab59bb9e33e93c60567ee92b3f66bea11
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-18 17:32:00 -0700

    Create Blurb component.

---
+++ src/Components/Blurb/Blurb.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Blurb.scss'
+
+let Blurb = () => {
+  return (
+    <section className='Blurb'>
+      <p>Test Test</p>
+      <p>Test Test</p> 
+    </section>
+  )
+}
+
+export default Blurb
