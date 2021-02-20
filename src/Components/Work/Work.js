fed0191d74fdaae886dec3422253f0f04bb001dd
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-20 14:34:58 -0700

    Build out more boxes in the component.

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -3,19 +3,28 @@ import React from 'react';
 import WorkBubble from './WorkBubble.js'
 const Work = () => {
   return (
-    <section className='Work' >
-      <div id='box' className='work1'></div>
-      <div id='box' className='work2'></div>
-      <div id='box' className='work3'></div>
-      <div id='box' className='work4'></div>
-      <div id='box' className='work5'></div>
-      <div id='box' className='work6'></div>
-      <div id='box' className='work7'></div>
-      <div id='box' className='work8'></div>
-      <div id='box' className='work9'></div>
-      <div id='box' className='work10'></div>
-      <div id='box' className='work11'></div>
-      <div id='box' className='work12'></div>
+    <section className='Work'>
+      <section className='WorkSection' >
+        <div id='box' className='box1'></div>
+        <div id='box' className='box2'></div>
+        <div id='box' className='box3'></div>
+        <div id='box' className='box4'></div>
+        <div id='box' className='box5'></div>
+        <div id='box' className='box6'></div>
+        <div id='box' className='box7'></div>
+        <div id='box' className='work1'></div>
+        <div id='box' className='work2'></div>
+        <div id='box' className='work3'></div>
+        <div id='box' className='work4'></div>
+        <div id='box' className='work5'></div>
+        <div id='box' className='work6'></div>
+        <div id='box' className='work7'></div>
+        <div id='box' className='work8'></div>
+        <div id='box' className='work9'></div>
+        <div id='box' className='work10'></div>
+        <div id='box' className='work11'></div>
+        <div id='box' className='work12'></div>
+      </section>
     </section>
   )
 }
