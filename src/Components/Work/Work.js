1075f4e4f6d0c324ec845288b205422d12265e33
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-20 15:31:06 -0700

    Convert some divs to Links to aid in tab naviagation.

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,6 +1,7 @@
 import './Work.scss'
 import React from 'react';
 import WorkBubble from './WorkBubble.js'
+import{Link} from 'react-router-dom'
 const Work = () => {
   return (
     <section className='Work'>
@@ -12,18 +13,18 @@ const Work = () => {
         <div id='box' className='box5'></div>
         <div id='box' className='box6'></div>
         <div id='box' className='box7'></div>
-        <div id='box' className='work1'></div>
-        <div id='box' className='work2'></div>
-        <div id='box' className='work3'></div>
-        <div id='box' className='work4'></div>
-        <div id='box' className='work5'></div>
-        <div id='box' className='work6'></div>
-        <div id='box' className='work7'></div>
-        <div id='box' className='work8'></div>
-        <div id='box' className='work9'></div>
-        <div id='box' className='work10'></div>
-        <div id='box' className='work11'></div>
-        <div id='box' className='work12'></div>
+        <Link id='box' className='work1'></Link>
+        <Link id='box' className='work2'></Link>
+        <Link id='box' className='work3'></Link>
+        <Link id='box' className='work4'></Link>
+        <Link id='box' className='work5'></Link>
+        <Link id='box' className='work6'></Link>
+        <Link id='box' className='work7'></Link>
+        <Link id='box' className='work8'></Link>
+        <Link id='box' className='work9'></Link>
+        <Link id='box' className='work10'></Link>
+        <Link id='box' className='work11'></Link>
+        <Link id='box' className='work12'></Link>
       </section>
     </section>
   )
