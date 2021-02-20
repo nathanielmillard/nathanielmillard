3c467df916d0938abccae6d03e13abf60f96694c
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-20 15:30:00 -0700

    Break nav bar down into less Links.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,15 +3,11 @@ import './NavBar.scss'
 import {Link} from "react-router-dom"
 let NavBar = () => {
   return (
-    <header className='NavBar'>
-        <Link to='/' className='home'>Nathaniel:</Link>
-      <nav>
-        <Link to='/resume'>resume,</Link>
-        <Link to='/work'>work,</Link>
-        <Link to='/background'>background,</Link>
-        <Link to='/contact'>contact</Link>
-      </nav>
-    </header>
+    <nav className='NavBar'>
+      <Link to='/resume'>resume, </Link>
+      <Link to='/background'>background, </Link>
+      <Link to='/contact'>contact</Link>
+    </nav>
   )
 }
 export default NavBar
