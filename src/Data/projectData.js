39f3ad373db7c09ccb7e88d966368a8f80777236
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-20 15:31:28 -0700

    Create outline for project data.

---
+++ src/Data/projectData.js
@@ -0,0 +1,24 @@
+export const projectData = {
+  'name': {
+    projectTitle: '',
+    projectDescription: '',
+    repoURl: '',
+    deployedURL: '',
+    contributors: [
+      {
+        name: '',
+        url: ''
+      }
+    ],
+    learningGoals: '',
+    technologyUsed: [
+      {
+        name: '',
+        url: ''
+      }
+    ],
+    videoURL: '',
+    photoURLs: [''],
+  },
+
+}
