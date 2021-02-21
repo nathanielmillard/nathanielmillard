9750962203a9059eca6ae5341838c5823547822f
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-20 17:46:59 -0700

    Start filling out Rume data.

--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -1,23 +1,47 @@
 export const projectData = {
-  'name': {
-    projectTitle: '',
-    projectDescription: '',
-    repoURl: '',
-    deployedURL: '',
+  'rume': {
+    projectTitle: 'Rume',
+    projectDescription: 'Rume was born of an interest in creating a design specific app that helped an already emotionally conscious user do more than "get past" or deescalate feelings.',
+    repoURl: 'https://github.com/nathanielmillard/rume',
+    deployedURL: 'https://rume-app.herokuapp.com/',
     contributors: [
       {
-        name: '',
-        url: ''
-      }
+        name: 'Kristi Miller',
+        url: 'https://github.com/Kristiannmiller'
+      },
+      {
+        name: 'Brigette Doelp',
+        url: 'https://github.com/BrigetteDoelp'
+      },
     ],
-    learningGoals: '',
+    learningGoals: 'Rume was a from scratch app that was focusing on dynamic user paths, integration of animations, multi-sensory UX/UI, and building a simple back end to pair with a more robust front end.',
     technologyUsed: [
       {
-        name: '',
-        url: ''
-      }
+        name: 'React',
+        url: 'https://reactjs.org/'
+      },
+      {
+        name: 'React Testing Library',
+        url: 'https://reactjs.org/'
+      },
+      {
+        name: 'Jest',
+        url: 'https://reactjs.org/'
+      },
+      {
+        name: 'Styled Components',
+        url: 'https://reactjs.org/'
+      },
+      {
+        name: 'GSAP',
+        url: 'https://reactjs.org/'
+      },
+      {
+        name: 'Node.js / Express',
+        url: 'https://reactjs.org/'
+      },
     ],
-    videoURL: '',
+    videoURL: 'https://www.youtube.com/watch?v=nlUTWOdFLR8',
     photoURLs: [''],
   },
 
